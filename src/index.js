bcfb1e23b9d305983e623423b3627e13491f34b1
SuperSpude <jamesbray90@hotmail.co.uk>
2020-07-20 14:07:12 +0100

    basic JS implementation of conways game of life

---
+++ src/index.js
@@ -0,0 +1,78 @@
+const height = 500, width = 500;
+const cellPerRow = 50;
+const rows = cellPerRow, cols = cellPerRow;
+const cellSize = height/cellPerRow;
+
+function generation(ctx, grid){
+    ctx.clearRect(0,0,width,height);
+    draw(ctx, grid);
+    grid = nextGeneration(grid);
+    requestAnimationFrame(() => generation(ctx, grid));
+}
+
+function nextGeneration(grid){
+    const nextGrid = new Array(grid.length);
+    for(let y = 0; y < cellPerRow; y++){
+        nextGrid[y] = new Array(grid.length);
+        for(let x = 0; x < cellPerRow; x++){
+            const cell = grid[y][x]; 
+            const neighbours = countNeighbours(grid, x, y);
+            if( cell == 1 && (neighbours < 2 || neighbours > 3)){
+                nextGrid[y][x] = 0;
+            } else if (cell == 0 && neighbours == 3) {
+                nextGrid[y][x] = 1;
+            } else {
+                nextGrid[y][x] = cell;
+            }
+        }
+    }
+    return nextGrid;
+}
+
+function countNeighbours(grid, cellX, cellY){
+    var count = 0;
+    for(let y = -1; y < 2; y++){
+        for(let x = -1; x < 2; x++){
+            var newY = (cellY + y + rows) % rows;
+            var newX = (cellX + x + cols) % cols;
+            if(grid[newY][newX] == 1){
+                count+=1;
+            }
+        }
+    }
+    return count - grid[cellY][cellX];
+}
+
+function getGrid(){
+    var grid = new Array(cellPerRow);
+    for(let y = 0; y < cellPerRow; y++){
+        grid[y] = new Array(cellPerRow);
+        for(let x = 0; x < cellPerRow; x++){
+                grid[y][x] = Math.round(Math.random());   
+        }
+    }
+    return grid;
+}
+
+function draw(ctx, grid){
+    for(let y = 0; y < cellPerRow; y++){
+        for(let x = 0; x < cellPerRow; x++){
+            if(grid[y][x] == 1){
+                ctx.fillStyle = "#000000";
+                ctx.fillRect(x*cellSize, y*cellSize, cellSize, cellSize);
+            } else {
+                ctx.strokeRect(x*cellSize, y*cellSize, cellSize, cellSize);
+            }
+        }
+    }
+}
+
+
+
+window.onload = () =>{
+    const c = document.getElementById("myCanvas");
+    const ctx = c.getContext("2d");
+    const grid = getGrid();
+    draw(ctx, grid);
+    generation(ctx, grid);
+}
